6134c36e10dc1b34d655313d0f59d28599d04d14
Marko <markogidej@yahoo.com>
2024-06-24 20:59:26 -0400

    Q1 done

--- doc.js
+++ doc.js
@@ -6,6 +6,23 @@ let products = [
 ];
 
 // Task 2: Write a function to display the product information on the webpage dynamically.
+function displayLaptopDetails() {
+    const paragraph = document.getElementById("display");
+    paragraph.innerHTML = `<strong>${products[0].name}</strong>: $${products[0].price}<br>${products[0].description}`;
+};
 
+function displaySmartphoneDetails() {
+    const paragraph = document.getElementById("display");
+    paragraph.innerHTML = `<strong>${products[1].name}</strong>: $${products[1].price}<br>${products[1].description}`;
+};
+
+function displayHeadphonesDetails() {
+    const paragraph = document.getElementById("display");
+    paragraph.innerHTML = `<strong>${products[2].name}</strong>: $${products[2].price}<br>${products[2].description}`;
+};
 
 // Task 3: Implement an event listener to trigger the display of products when the page loads.
+document.getElementById("start").addEventListener("click", function() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("container").style.display = "block";
+});
